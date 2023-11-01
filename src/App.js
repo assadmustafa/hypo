a62d770cdc98bff988d6fb85e50aa96870afa2d9
assadmustafa <1168379@student.roc-nijmegen.nl>
2023-11-01 21:20:02 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,200 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      income: '',
+      partnerIncome: '',
+      monthlyDebt: '',
+      term: '30',
+      hasStudentDebt: false,
+      zipCode: '',
+      maxMortgage: 0,
+      errorMessage: '',
+      monthlyPayment: 0,
+      interestPerMonth: 0,
+      principalPerMonth: 0,
+      totalCost: 0,
+    };
+  }
+
+  handleIncomeChange = (event) => {
+    this.setState({ income: event.target.value });
+  }
+
+  handlePartnerIncomeChange = (event) => {
+    this.setState({ partnerIncome: event.target.value });
+  }
+
+  handleMonthlyDebtChange = (event) => {
+    this.setState({ monthlyDebt: event.target.value });
+  }
+
+  handleTermChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  handleStudentDebtChange = (event) => {
+    this.setState({ hasStudentDebt: event.target.checked });
+  }
+
+  handleZipCodeChange = (event) => {
+    this.setState({ zipCode: event.target.value });
+  }
+
+  calculateMaxMortgage = () => {
+    const { income, partnerIncome, monthlyDebt, term, hasStudentDebt, zipCode } = this.state;
+  
+    if (!income || !partnerIncome || !monthlyDebt || !zipCode) {
+      this.setState({ errorMessage: 'Please fill in all required fields.' });
+      return;
+    }
+  
+    const incomeNum = parseFloat(income);
+    const partnerIncomeNum = parseFloat(partnerIncome);
+    const monthlyDebtNum = parseFloat(monthlyDebt);
+    const termInYears = parseInt(term);
+  
+    if (!isNaN(incomeNum) && !isNaN(monthlyDebtNum) && !isNaN(termInYears)) {
+      const zipCodesToReject = ['9679', '9681', '9682'];
+      if (zipCodesToReject.includes(zipCode)) {
+        this.setState({ errorMessage: 'Mortgages in this area are not accepted due to earthquake risk and declining home values.' });
+      } else {
+        const monthsInYear = 12;
+        let interestRate;
+  
+        switch (termInYears) {
+          case 2:
+            interestRate = 0.02;
+            break;
+          case 5:
+            interestRate = 0.03;
+            break;
+          case 10:
+            interestRate = 0.035;
+            break;
+          case 20:
+            interestRate = 0.045;
+            break;
+          case 30:
+            interestRate = 0.05;
+            break;
+          default:
+            interestRate = 0.04;
+            break;
+        }
+  
+        const totalIncome = incomeNum + partnerIncomeNum;
+        const monthlyRate = interestRate / monthsInYear;
+        const totalMonths = termInYears * monthsInYear;
+  
+        let maxMortgage = ((totalIncome * 0.28 - monthlyDebtNum) / (1 - Math.pow(1 + monthlyRate, -totalMonths))).toFixed(2);
+        
+
+        if (hasStudentDebt) {
+          maxMortgage *= 0.75;
+        }
+  
+        this.setState({ maxMortgage, errorMessage: '' });
+  
+        const monthlyPayment = (maxMortgage * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -totalMonths));
+        const totalCost = monthlyPayment * totalMonths;
+        const interestPerMonth = monthlyPayment - (maxMortgage / totalMonths);
+        const principalPerMonth = maxMortgage / totalMonths;
+  
+        this.setState({ monthlyPayment, interestPerMonth, principalPerMonth, totalCost });
+  
+        // Clear the error message when calculations are successful
+        this.setState({ errorMessage: '' });
+      }
+    }
+  }
+  
+
+  render() {
+    return (
+      <div className="calculator">
+        <h2>Hypo</h2>
+        <div className="input-container">
+          <label>Your Monthly Income:</label>
+          <input
+            type="number"
+            value={this.state.income}
+            onChange={this.handleIncomeChange}
+            data-testid="monthly-income"
+          />
+        </div>
+        <div className="input-container">
+          <label>Partner's Monthly Income:</label>
+          <input
+            type="number"
+            value={this.state.partnerIncome}
+            onChange={this.handlePartnerIncomeChange}
+            data-testid="partner-income"
+          />
+        </div>
+        <div className="input-container">
+          <label>Monthly Debt Payments:</label>
+          <input
+            type="number"
+            value={this.state.monthlyDebt}
+            onChange={this.handleMonthlyDebtChange}
+            data-testid="monthly-payment"
+          />
+        </div>
+        <div className="input-container">
+          <label>Mortgage Term (years):</label>
+          <select
+            value={this.state.term}
+            onChange={this.handleTermChange}
+            data-testid="term"
+          >
+            <option value="2">2</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+          </select>
+        </div>
+        <div className="input-container">
+          <label>Zip Code:</label>
+          <input
+            type="text"
+            value={this.state.zipCode}
+            onChange={this.handleZipCodeChange}
+            data-testid="zipcode"
+          />
+        </div>
+        <div className="input-container">
+          <label>Do you have student debt?</label>
+          <input
+            type="checkbox"
+            checked={this.state.hasStudentDebt}
+            onChange={this.handleStudentDebtChange}
+            data-testid="student-dept"
+          />
+        </div>
+        <button onClick={this.calculateMaxMortgage}>Calculate Max Mortgage</button>
+        <div className="result">
+          {this.state.errorMessage ? (
+            <p className="error-message">{this.state.errorMessage}</p>
+          ) : (
+            this.state.maxMortgage > 0 && (
+              <div>
+                <p data-testid="max-mortgage">Maximum Allowed Mortgage: ${this.state.maxMortgage}</p>
+                <p data-testid="monthly-payment-result">Monthly Payment: ${this.state.monthlyPayment.toFixed(2)}</p>
+                <p data-testid="interest-per-month">Interest Paid Each Month: ${this.state.interestPerMonth.toFixed(2)}</p>
+                <p data-testid="principal-per-month">Principal Repaid Each Month: ${this.state.principalPerMonth.toFixed(2)}</p>
+                <p data-testid="total-cost">Total Cost Over the Full Term: ${this.state.totalCost.toFixed(2)}</p>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,65 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import { render, screen, fireEvent } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Unit Tests', () => {
+    render(<App/>);
+
+    const incomeInput = screen.getByTestId('monthly-income');
+    const partnerIncomeInput = screen.getByTestId('partner-income');
+    const monthlyDebtInput = screen.getByTestId('monthly-payment');
+    const termInput = screen.getByTestId('term');
+    const zipCodeInput = screen.getByTestId('zipcode');
+    const studentDebtCheckbox = screen.getByTestId('student-dept');
+    const calculateButton = screen.getByText('Calculate Max Mortgage');
+
+    // Simulate user input by changing the value of the input field
+    fireEvent.change(incomeInput, { target: { value: '25000' } });
+    fireEvent.change(partnerIncomeInput, { target: { value: '15000' } });
+    fireEvent.change(monthlyDebtInput, { target: { value: '500' } });
+    fireEvent.change(termInput, { target: { value: '20' } });
+    fireEvent.change(zipCodeInput, { target: { value: '6641' } });
+    fireEvent.click(studentDebtCheckbox);
+    fireEvent.click(calculateButton);
+
+    expect(incomeInput.value).toBe('25000');
+    expect(partnerIncomeInput.value).toBe('15000');
+    expect(monthlyDebtInput.value).toBe('500');
+    expect(termInput.value).toBe('20');
+    expect(zipCodeInput.value).toBe('6641');
+    expect(studentDebtCheckbox).toBeChecked();
+    
+})
+
+test('Integration Test', () => {
+    render(<App/>);
+
+    const incomeInput = screen.getByTestId('monthly-income');
+    const partnerIncomeInput = screen.getByTestId('partner-income');
+    const monthlyDebtInput = screen.getByTestId('monthly-payment');
+    const termInput = screen.getByTestId('term');
+    const zipCodeInput = screen.getByTestId('zipcode');
+    const studentDebtCheckbox = screen.getByTestId('student-dept');
+    const calculateButton = screen.getByText('Calculate Max Mortgage');
+
+    fireEvent.change(incomeInput, { target: { value: '25000' } });
+    fireEvent.change(partnerIncomeInput, { target: { value: '15000' } });
+    fireEvent.change(monthlyDebtInput, { target: { value: '500' } });
+    fireEvent.change(termInput, { target: { value: '20' } });
+    fireEvent.change(zipCodeInput, { target: { value: '6641' } });
+    fireEvent.click(studentDebtCheckbox);
+    fireEvent.click(calculateButton);
+
+    // Assertions for the results
+    const maxMortgageResult = screen.getByTestId('max-mortgage').textContent;
+    const monthlyPaymentResult = screen.getByTestId('monthly-payment-result').textContent;
+    const interestPerMonthResult = screen.getByTestId('interest-per-month').textContent;
+    const principalPerMonthResult = screen.getByTestId('principal-per-month').textContent;
+    const totalCostResult = screen.getByTestId('total-cost').textContent;
+
+    // Expect the result values to match the expected values
+    expect(maxMortgageResult).toBe('Maximum Allowed Mortgage: $13538.699999999999');
+    expect(monthlyPaymentResult).toBe('Monthly Payment: $85.65');
+    expect(interestPerMonthResult).toBe('Interest Paid Each Month: $29.24');
+    expect(principalPerMonthResult).toBe('Principal Repaid Each Month: $56.41');
+    expect(totalCostResult).toBe('Total Cost Over the Full Term: $20556.60');
+})
